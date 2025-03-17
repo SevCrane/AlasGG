3d90408e2a5dc1096041e71c2573ceba8a6d8935
SevCrane <seven.crane.zh@gmail.com>
2025-03-17 19:29:13 +0800

    DEV

--- module/gg_handler/gg_handler.py
+++ module/gg_handler/gg_handler.py
@@ -1,9 +1,45 @@
 from module.gg_handler.gg_data import GGData
 from module.gg_handler.gg_u2 import GGU2
 # from module.gg_handler.gg_screenshot import GGScreenshot
-from module.config.utils import deep_get, deep_set
 from module.logger import logger
 from module.base.timer import timeout
+import module.config.utils as utils
+
+# 如果 utils 模块中没有 deep_get 和 deep_set，则动态定义并注入
+if not (hasattr(utils, 'deep_get') and hasattr(utils, 'deep_set')):
+    def deep_get(d, keys, default=None):
+        """
+        递归安全地获取嵌套字典的值
+        """
+        if isinstance(keys, str):
+            keys = keys.split('.')
+        assert isinstance(keys, list)
+        if d is None:
+            return default
+        if not keys:
+            return d
+        return deep_get(d.get(keys[0]), keys[1:], default)
+
+    def deep_set(d, keys, value):
+        """
+        递归安全地设置嵌套字典的值
+        """
+        if isinstance(keys, str):
+            keys = keys.split('.')
+        assert isinstance(keys, list)
+        if not keys:
+            return value
+        if not isinstance(d, dict):
+            d = {}
+        d[keys[0]] = deep_set(d.get(keys[0], {}), keys[1:], value)
+        return d
+
+    # 将定义好的函数添加到 utils 模块中
+    utils.deep_get = deep_get
+    utils.deep_set = deep_set
+
+# 然后再从 utils 模块导入 deep_get 和 deep_set
+from module.config.utils import deep_get, deep_set
 
 
 class GGHandler:
@@ -13,6 +49,8 @@ class GGHandler:
         config: AzurlaneConfig
         device: Device
     """
+
+       
 
     def __init__(self, config=None, device=None):
         self.config = config
