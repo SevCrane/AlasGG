d72ee04d79dcb74edfe8522335298ff36c382c6f
SevCrane <seven.crane.zh@gmail.com>
2025-03-17 19:56:39 +0800

    dev

--- module/webui/app.py
+++ module/webui/app.py
@@ -482,7 +482,7 @@ class AlasGUI(Frame):
         self.task_handler.add(log.put_log(self.alas), 0.25, True)
 
     def set_dashboard_display(self, b):
-        self._log.set_dashboard_display(b)
+        self._log.set_dashboard_display(True)
         self.alas_update_dashboard(True)
 
     def _init_alas_config_watcher(self) -> None:
